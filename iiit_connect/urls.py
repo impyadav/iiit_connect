0ee472c84fc6c7257dff96b3de88f201ce633e9c
Prateek Yadav <pyadav223141@gmail.com>
2016-11-18 02:56:32 +0530

    Update urls.py

--- iiit_connect/urls.py
+++ iiit_connect/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     
     # url(r'^simpleemail/(?P<emailto>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/',views.sendSimpleEmail, name = 'sendSimpleEmail')
-     url(r'^massemail/(?P<emailto1>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<emailto2>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})',views.sendMassEmail, name = 'sendMassEmail')
+     #url(r'^massemail/(?P<emailto1>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<emailto2>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<string>[\w\-]+)/$',views.sendMassEmail, name = 'sendMassEmail')
+     url(r'^massemail2/', views.post_req , name = 'post_req')
 ]
