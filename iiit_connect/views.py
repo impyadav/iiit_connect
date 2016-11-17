ac88afe289412c5dff2a95258b5a766537a8bdb5
Prateek Yadav <pyadav223141@gmail.com>
2016-11-18 01:49:15 +0530

    Update views.py

--- iiit_connect/views.py
+++ iiit_connect/views.py
@@ -1,7 +1,10 @@
+import json
+
 from  django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mass_mail
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,7 +17,7 @@ from .serializers import EmailInvitationSerializer
 #invitation/
 class InvitationList(APIView):
 
-	def get(self , request):
+	def get(self, request):
 		invitation = EmailInvitation.objects.all()
 		serializer = EmailInvitationSerializer( invitation , many = True)
 		return Response(serializer.data)
@@ -29,3 +32,16 @@ def sendMassEmail(request,emailto1,emailto2) :
      msg2 = ('sub2','msg2','201451041@iiitvadodara.ac.in',['201451011@iiitvadodara.ac.in'])
      res = send_mass_mail((msg1,msg2),fail_silently = False)
      return HttpResponse('%s'%res)
+
+
+@csrf_exempt
+def post_req(request):
+	from_mail = request.POST.get('from')
+	to_mail = request.POST.get('to')
+	message = request.POST.get('message')
+	u_code = request.POST.get('uniquecode')
+	e_name = request.POST.get('eventname')
+	mail_subject = 'Invitation to Join Event {0}'.format(e_name.capitalize())
+	mail_body = '{0} sent you an invitation to join event {1}.\n{2}\nUnique code for the event is {3}'.format(from_mail, e_name, message, u_code)
+	response = send_mass_mail(((mail_subject, mail_body, from_mail, [to_mail]),), fail_silently=True)
+	return json.dumps({'response': response})
